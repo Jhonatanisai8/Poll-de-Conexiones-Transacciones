066f3c34410437020d83cd2b19633908e1f5b66a
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-09 21:38:34 -0500

    clase servivio: algunos metodos que luego implementaremos

---
+++ src/main/java/org/jhonatan/jdbc/servicio/Servicio.java
@@ -0,0 +1,27 @@
+package org.jhonatan.jdbc.servicio;
+
+import java.sql.SQLException;
+import java.util.List;
+import org.jhonatan.jdbc.modelo.Categoria;
+import org.jhonatan.jdbc.modelo.Producto;
+
+public interface Servicio {
+
+    List<Producto> listar() throws SQLException;
+
+    Producto porId(Long id) throws SQLException;
+
+    Producto guardar(Producto producto) throws SQLException;
+
+    void eliminar(Long id) throws SQLException;
+
+    List<Categoria> listarCategorias() throws SQLException;
+
+    Categoria porIdCategoria(Long id) throws SQLException;
+
+    Categoria guardarCategoria() throws SQLException;
+
+    void eliminarCategoria(Long id) throws SQLException;
+
+    void guardarProductoConCategoria(Producto p, Categoria c) throws SQLException;
+}
